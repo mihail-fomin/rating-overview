199386a592c25ccd9f5fee57787b56e6ec8122f7
Mihail Fomin <m.fomin@quickresto.ru>
2024-05-03 17:35:36 +0500

    Added detailed page

---
+++ app/cards/[id]/page.tsx
@@ -0,0 +1,44 @@
+import prisma from '@/app/utlis/connect'
+import React from 'react'
+import { cache } from 'react'
+import { notFound } from 'next/navigation'
+import { getServerSession } from 'next-auth'
+import authOptions from '@/app/auth/authOptions'
+import { Text, Box, Flex } from '@radix-ui/themes'
+import Link from 'next/link'
+
+type Props = { params: { id: string } }
+
+const fetchWorker = cache((workerId: string) => prisma.worker.findUnique({ where: { id: workerId } }))
+
+const WorkerDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions)
+
+  const worker = await fetchWorker(params.id)
+  if (!worker) notFound()
+
+  return (
+    <>
+      {session && (
+        <Box>
+          <Flex gap="4" mt="2">
+            <Link href={`/cards/edit/${worker.id}`}>Изменить</Link>
+            <Link href={`/cards/delete/${worker.id}`}>Удалить</Link>
+          </Flex>
+        </Box>
+      )}
+      <Text as="p">{worker.fullName}</Text>
+    </>
+  )
+}
+
+export async function generateMetadata({ params }: Props) {
+  const worker = await fetchWorker(params.id)
+
+  return {
+    title: worker?.fullName,
+    description: 'Данные рабочего ' + worker?.id,
+  }
+}
+
+export default WorkerDetailPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Card, Text, Flex } from '@radix-ui/themes'
 import prisma from './utlis/connect'
-import Worker from '@prisma/client'
+import { Worker } from '@prisma/client'
+import Link from 'next/link'
 
 export default async function Home() {
   const workers: Worker[] = await prisma.worker.findMany()
@@ -9,11 +10,16 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-4">
       <Flex gap="3">
         {workers.map((worker: Worker) => (
-          <Card key={worker.id}>
-            <Text as="p">{worker.fullName}</Text>
-            <Text as="p">{worker.birthDate.toDateString()}</Text>
-            <Text as="p">{worker.phone}</Text>
-          </Card>
+          <Link key={worker.id} href={`/cards/${worker.id}`}>
+            <Card className="min-w-[16rem] p-2">
+              <Text as="p">{worker.fullName}</Text>
+              <Text as="p">{worker.birthDate.toLocaleString()}</Text>
+              <Text as="p">{worker.phone}</Text>
+              <Text as="p">{worker.department}</Text>
+              <Text as="p">{worker.position}</Text>
+              <Text as="p">{worker.ranking}</Text>
+            </Card>
+          </Link>
         ))}
       </Flex>
     </main>
