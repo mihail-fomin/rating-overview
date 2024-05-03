84272ea32ad4a094cf27da4a5343e8bc77b6f570
Mihail Fomin <m.fomin@quickresto.ru>
2024-05-03 13:09:51 +0500

    Added cards on dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <main className="flex min-h-screen flex-col items-center justify-between p-24">Home page</main>
+import { Card, Text, Flex } from '@radix-ui/themes'
+import prisma from './utlis/connect'
+import Worker from '@prisma/client'
+
+export default async function Home() {
+  const workers: Worker[] = await prisma.worker.findMany()
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      <Flex gap="3">
+        {workers.map((worker: Worker) => (
+          <Card key={worker.id}>
+            <Text as="p">{worker.fullName}</Text>
+            <Text as="p">{worker.birthDate.toDateString()}</Text>
+            <Text as="p">{worker.phone}</Text>
+          </Card>
+        ))}
+      </Flex>
+    </main>
+  )
 }
