3fb7d1e543847e7baf238ec7773528dff1f54e0f
Mihail Fomin <m.fomin@quickresto.ru>
2024-05-06 12:25:49 +0500

    Secured pages from unauthrized

--- app/cards/edit/[id]/page.tsx
+++ app/cards/edit/[id]/page.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import UserForm from '../../_components/UserForm'
 import prisma from '@/app/utlis/connect'
+import { getServerSession } from 'next-auth'
+import authOptions from '@/app/auth/authOptions'
+import { redirect } from 'next/navigation'
 
+const EditWorkerPage = async ({ params }: { params: { id: string } }) => {
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    redirect('/')
+  }
 
+  const worker = await prisma.worker.findUnique({ where: { id: params.id } })
 
-const EditWorkerPage = async ({ params }: {params: {id: string}}) => {
-  const worker = await prisma.worker.findUnique({where: { id: params.id}})
-  
-  return <UserForm worker={worker || undefined}/>}
+  return <UserForm worker={worker || undefined} />
+}
 
 export default EditWorkerPage
--- app/cards/new/page.tsx
+++ app/cards/new/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import UserForm from '../_components/UserForm'
+import { getServerSession } from 'next-auth'
+import authOptions from '@/app/auth/authOptions'
+import { redirect } from 'next/navigation'
 
+const NewWorkerPage = async () => {
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    redirect('/')
+  }
 
-const NewWorkerPage = () => {
-
-  return <UserForm/>
+  return <UserForm />
 }
 
 export default NewWorkerPage
