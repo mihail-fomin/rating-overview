5b87254cadd4189796775844a61f51b0c41f3d32
Mihail Fomin <m.fomin@quickresto.ru>
2024-05-06 18:08:41 +0500

    Added PATCH route

---
+++ app/api/cards/[id]/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from 'next/server'
+import prisma from '@/app/utlis/connect'
+import { getServerSession } from 'next-auth'
+import authOptions from '@/app/auth/authOptions'
+
+export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
+  const session = await getServerSession(authOptions)
+  if (!session) return NextResponse.json({}, { status: 401 })
+
+  const { fullName, birthDate, phone, department, position, ranking } = await req.json()
+
+  const worker = await prisma.worker.findUnique({ where: { id: params.id } })
+
+  if (!worker) {
+    return NextResponse.json({ error: 'Worker not found' }, { status: 404 })
+  }
+
+  const updatedWorker = await prisma.worker.update({
+    where: { id: params.id },
+    data: { fullName, birthDate, phone, department, position, ranking: Number(ranking) },
+  })
+
+  return NextResponse.json(updatedWorker)
+}
