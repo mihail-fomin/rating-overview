34858228a475eecaba7a183c173e4f2fe56529ba
Mihail Fomin <m.fomin@quickresto.ru>
2024-05-06 18:09:47 +0500

    Refactor: Edited userform fields

--- app/cards/_components/UserForm.tsx
+++ app/cards/_components/UserForm.tsx
@@ -3,14 +3,29 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
-import { useForm } from 'react-hook-form'
+import { Controller, useForm, useFormContext } from 'react-hook-form'
 import toast from 'react-hot-toast/headless'
 import { Button, Select, TextField } from '@radix-ui/themes'
 import { Worker } from '@prisma/client'
 
-type Props = {}
+interface DepartmentSelectProps {
+  name: string
+  control: any
+  defaultValue: string
+}
 
-const UserForm = ({ worker }: { worker?: Worker}) => {
+const departments = [
+  { label: 'Клиентский', value: 'CLIENT' },
+  { label: 'Продажи', value: 'SALES' },
+  { label: 'Разработка', value: 'DEVELOPMENT' },
+]
+
+const positions = [
+  { label: 'Сотрудник', value: 'WORKER' },
+  { label: 'Руководитель', value: 'LEADER' },
+]
+
+const UserForm = ({ worker }: { worker?: Worker }) => {
   const router = useRouter()
   const {
     register,
@@ -27,42 +42,82 @@ const UserForm = ({ worker }: { worker?: Worker}) => {
       setSubmitting(true)
       if (worker) {
         await axios.patch('/api/cards/' + worker.id, data)
-        toast.success('Задача была обновлена')
+        toast.success('Карточка была обновлена')
       } else {
         await axios.post('/api/cards', data)
-        toast.success('Задача была создана')
+        toast.success('Карточка была создана')
       }
-      router.push('/cards/list')
+      router.push('/')
       router.refresh()
     } catch (error) {
       setSubmitting(false)
       setError('Произошла внезапная ошибка((')
-      toast.error('Задача не может быть создана')
+      toast.error('Карточка не может быть создана')
     }
   })
 
   return (
-    <form className='max-w-xl flex flex-col gap-2'>
-      <TextField.Root  defaultValue={worker?.fullName} placeholder="Имя" {...register('fullName')}/>
-      <TextField.Root  defaultValue={worker?.birthDate.toLocaleDateString()} placeholder="Дата" {...register('birthDate')}/>
-      <TextField.Root  defaultValue={worker?.phone} placeholder="Телефон" {...register('phone')}/>
-      <Select.Root defaultValue='123'>
-        <Select.Trigger />
-        <Select.Content>
-          {worker?.department.map()}
-            <Select.Item value="orange">Orange</Select.Item>
-            <Select.Item value="apple">Apple</Select.Item>
-            <Select.Item value="grape" disabled>
-              Grape
-            </Select.Item>
-        </Select.Content>
-      </Select.Root>
-      <TextField.Root  defaultValue={worker?.department} placeholder="Отделение" {...register('department')}/>
-      <TextField.Root  defaultValue={worker?.position} placeholder="Должность" {...register('position')}/>
-      <TextField.Root  defaultValue={worker?.ranking} placeholder="Рейтинг" {...register('ranking')}/>
-      <Button type='submit'>Отправить</Button>
+    <form className="max-w-xl flex flex-col gap-2" onSubmit={onSubmit}>
+      <TextField.Root defaultValue={worker?.fullName} placeholder="Имя" {...register('fullName')} />
+      <TextField.Root
+        defaultValue={worker?.birthDate.toLocaleDateString()}
+        placeholder="Дата"
+        {...register('birthDate')}
+      />
+      <TextField.Root defaultValue={worker?.phone} placeholder="Телефон" {...register('phone')} />
+
+      <SelectField
+        name="department"
+        placeholder="Отдел"
+        control={control}
+        defaultValue={worker?.department}
+        options={departments}
+      />
+      <SelectField
+        name="position"
+        placeholder="Позиция"
+        control={control}
+        defaultValue={worker?.position}
+        options={positions}
+      />
+
+      <TextField.Root defaultValue={worker?.ranking} placeholder="Рейтинг" {...register('ranking')} />
+      <Button type="submit">Отправить</Button>
     </form>
   )
 }
 
 export default UserForm
+
+interface SelectFieldProps {
+  name: string
+  placeholder: string
+  control: any
+  defaultValue: string | undefined
+  options: { label: string; value: string }[]
+}
+
+const SelectField = React.forwardRef<HTMLDivElement, SelectFieldProps>(
+  ({ name, placeholder, control, defaultValue, options }, ref) => {
+    return (
+      <Controller
+        name={name}
+        control={control}
+        defaultValue={defaultValue}
+        render={({ field }) => (
+          // @ts-ignore
+          <Select.Root {...field} onValueChange={field.onChange} ref={ref}>
+            <Select.Trigger placeholder={placeholder} />
+            <Select.Content>
+              {options.map((option) => (
+                <Select.Item key={option.value} value={option.value}>
+                  {option.label}
+                </Select.Item>
+              ))}
+            </Select.Content>
+          </Select.Root>
+        )}
+      />
+    )
+  },
+)
